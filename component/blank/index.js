8f1b2a8ca394063765532f48097c06068c427783
lixiangqun <1547843445@qq.com>
2019-03-15 11:23:49 +0800

    无数据的空白页

---
+++ component/blank/index.js
@@ -0,0 +1,30 @@
+// component/blank/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    text: {
+      type: String,
+      value: ''
+    },
+    src: {
+      type: String,
+      value: ''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
