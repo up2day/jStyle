cf3289e107b6e49d3f406899294f30e38e648ffc
lixiangqun <1547843445@qq.com>
2019-03-17 08:35:34 +0800

    图片

---
+++ component/aside/index.js
@@ -0,0 +1,56 @@
+// component/aside/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    list: [
+      {
+        src: '../../assets/images/search.png',
+        srcOn: '../../assets/images/search.png',
+        path: '跳转路径'
+      },
+      {
+        src: '../../assets/images/calculate.png',
+        srcOn: '../../assets/images/calculate.png',
+        path: '跳转路径'
+      },
+      {
+        src: '../../assets/images/clothes.png',
+        srcOn: '../../assets/images/clothes.png',
+        path: '跳转路径'
+      },
+      {
+        src: '../../assets/images/food.png',
+        srcOn: '../../assets/images/food.png',
+        path: '跳转路径'
+      },
+      {
+        src: '../../assets/images/living.png',
+        srcOn: '../../assets/images/living.png',
+        path: '跳转路径'
+      },
+      {
+        src: '../../assets/images/car.png',
+        srcOn: '../../assets/images/car.png',
+        path: '跳转路径'
+      },
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onJump(e){
+      
+    }
+  }
+})
